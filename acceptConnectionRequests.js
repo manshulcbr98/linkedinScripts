2523cbdf724209970b2d13495239965ed28f55c8
manshulcbr98 <43809116+manshulcbr98@users.noreply.github.com>
2022-11-23 19:10:10 +0530

    Create acceptConnectionRequests.js

---
+++ acceptConnectionRequests.js
@@ -0,0 +1,10 @@
+// Accept all connection requests.
+
+var array = document.querySelectorAll('span');
+var searchString = “Connect”;
+for(var i = 0; i < array.length; i = i + 1) { 
+    var text1 = array[i].textContent; 
+    if ( text1.includes(searchString)){
+        await new Promise(r => setTimeout(r, 2000)); array[i].click();;
+    }
+}

df5d4ef8910aa72af60f470c82b4f64bb399e63f
manshulcbr98 <43809116+manshulcbr98@users.noreply.github.com>
2022-11-29 19:13:32 +0530

    Update acceptConnectionRequests.js
    corrected the double quotes

--- acceptConnectionRequests.js
+++ acceptConnectionRequests.js
@@ -1,7 +1,7 @@
 // Accept all connection requests.
 
 var array = document.querySelectorAll('span');
-var searchString = “Connect”;
+var searchString = "Accept";
 for(var i = 0; i < array.length; i = i + 1) { 
     var text1 = array[i].textContent; 
     if ( text1.includes(searchString)){

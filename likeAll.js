0ebb4ea9e81deed07e6d38a7e6ef1b71738ed52b
manshulcbr98 <43809116+manshulcbr98@users.noreply.github.com>
2023-01-19 22:35:40 +0530

    Update likeAll.js

--- likeAll.js
+++ likeAll.js
@@ -2,6 +2,19 @@ var array = document.getElementsByClassName('feed-shared-social-actions feed-sha
 var searchString = "Like";
 for(var i = 0; i < array.length; i = i + 1) {
     var array2 = array[i].querySelectorAll('span');
+        text1 = array2[1].textContent;
+        if ( text1.includes(searchString)){
+            await new Promise(r => setTimeout(r, 2000)); array2[1].click();;
+        }
+    //}
+}
+
+
+////////////
+var array = document.getElementsByClassName('feed-shared-social-actions feed-shared-social-action-bar social-detail-base-social-actions feed-shared-social-action-bar--full-width feed-shared-social-action-bar--has-social-counts');
+var searchString = "Like";
+for(var i = 0; i < array.length; i = i + 1) {
+    var array2 = array[i].querySelectorAll('span');
     await new Promise(r => setTimeout(r, 2000)); array2[1].click();;
     if ( text1.includes(searchString)){
         await new Promise(r => setTimeout(r, 2000)); array[i].click();;
